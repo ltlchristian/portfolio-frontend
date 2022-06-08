b1a16f00d2ba38fc376474fd894b07b4bba2a4df
Christian ly <christian_ly@outlook.fr>
2022-06-09 01:36:01 +0200

    Ajout update infos site

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import services from "../../services";
@@ -8,6 +8,9 @@ function Admin() {
   const [body, setBody] = useState({
     label: "",
   });
+
+  const [presentation, setPresentation] = useState({});
+
   const navigate = useNavigate();
 
   function updateBody(key, value) {
@@ -27,18 +30,61 @@ function Admin() {
       .then(() => navigate("/"))
       .catch(() => alert("Une erreur a eu lieu pendant la création d'une techno"));
   }
-  
+
+  function updateSite(key, value) {
+    setPresentation({ ...presentation, [key]: value });
+  }
+
+  function handleFormChangeSite(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    updateSite(name, value);
+  }
+
+  function handleSubmitSite(event) {
+    event.preventDefault();
+
+    const { presentation_titre, presentation_sum } = presentation;
+
+    const updatedSite = { presentation_titre, presentation_sum };
+    services
+      .updateSite(presentation._id, updatedSite)
+      .catch(() => alert("Une erreur pendant la mise à jour des infos du site"));
+  }
+
+  useEffect(() => {
+    services
+      .getInfos()
+      .then((response) => {
+        setPresentation(response);
+      })
+      .catch(console.log);
+  }, []);
+
   return (
     <div>
       <h1>Admin</h1>
       <Container>
         <h2>Technos</h2>
-        <Form onSubmit={handleSubmitTechnos} onChange={handleFormChangeTechnos} >
+        <Form onSubmit={handleSubmitTechnos}>
             <Form.Group className="mb-3" controlId="label">
               <Form.Label>label</Form.Label>
-              <Form.Control type="text" placeholder="libellé techno" name="label" required/>
+              <Form.Control type="text" placeholder="libellé techno" name="label" onChange={handleFormChangeTechnos} required/>
             </Form.Group>
             <Button variant="primary" type="submit">Ajouter</Button>
+        </Form>
+        <hr/>
+        <h2>Site</h2>
+        <Form onSubmit={handleSubmitSite}>
+            <Form.Group className="mb-3" controlId="presentation_titre">
+              <Form.Label>presentation_titre</Form.Label>
+              <Form.Control type="text" placeholder="un titre" name="presentation_titre" value={presentation.presentation_titre} onChange={handleFormChangeSite} required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="presentation_sum">
+              <Form.Label>presentation_sum</Form.Label>
+              <Form.Control type="text" placeholder="un texte" name="presentation_sum" value={presentation.presentation_sum} onChange={handleFormChangeSite} required/>
+            </Form.Group>            
+            <Button variant="primary" type="submit">Modifier</Button>
         </Form>
       </Container>
     </div>
--- src/services.js
+++ src/services.js
@@ -83,6 +83,15 @@ const services = {
       })
       .then((res) => res.data);
   },
+
+  updateSite(idSite, body) {
+    const token = localStorage.getItem("jwt");
+    return base
+      .post(`/sites/${idSite}`, body, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => res.data);
+  },
 };
 
 export default services;
